4ff3727e56a9ba2b84a1f06180ef5e3d141fbd1b
Hairong Zheng <115609571+hairoom@users.noreply.github.com>
2024-09-23 14:51:48 +0800

    Update app-housing.py

--- app-housing.py
+++ app-housing.py
@@ -12,7 +12,7 @@ df = pd.read_csv('housing.csv')
 
 price_filter = st.slider('Minimal Median House Price:', 0, 500001, 200000)
 
-capital_filter = st.sidebar.multiselect(
+location_filter = st.sidebar.multiselect(
      'Choose the location type',
      df.ocean_proximity.unique(),  # options
      df.ocean_proximity.unique())  # defaults
@@ -23,6 +23,7 @@ income_filter = st.sidebar.radio('Choose income level',
                           ('Low','Median','High'))
 
 df=df[df.median_house_value >= price_filter]
+df = df[df.ocean_proximity.isin(location_filter)]
 
 if  income_filter=='Low':
       df=df[df.median_income<=2.5]
